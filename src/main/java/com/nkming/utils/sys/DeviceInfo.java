ac88b82b3c0e1e4ad7b8261e16a33e98f8962d2a
Ming Ming <mint2648@gmail.com>
2015-04-07 05:58:32 +0800

    Retrieving full screen size

--- src/main/java/com/nkming/utils/sys/DeviceInfo.java
+++ src/main/java/com/nkming/utils/sys/DeviceInfo.java
@@ -9,10 +9,17 @@
 package com.nkming.utils.sys;
 
 import android.content.Context;
+import android.graphics.Point;
+import android.os.Build;
 import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
 
+import com.nkming.utils.Log;
 import com.nkming.utils.type.Size;
 import com.nkming.utils.type.SizeF;
+
+import java.lang.reflect.Method;
 
 /***
  * Retrieve various device information
@@ -56,4 +63,46 @@ public class DeviceInfo
 		DisplayMetrics dm = context.getResources().getDisplayMetrics();
 		return new Size(dm.widthPixels, dm.heightPixels);
 	}
+
+	/**
+	 * Get the device screen size in pixel, including the navigation bar
+	 *
+	 * @param context
+	 * @return
+	 */
+	public static Size GetFullScreenPx(Context context)
+	{
+		WindowManager wm = (WindowManager)context.getSystemService(
+				Context.WINDOW_SERVICE);
+		Display d = wm.getDefaultDisplay();
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+		{
+			Point p = new Point();
+			d.getRealSize(p);
+			return new Size(p.x, p.y);
+		}
+		else
+		{
+			try
+			{
+				Method methodGetRawWidth = Display.class.getMethod("getRawWidth");
+				methodGetRawWidth.setAccessible(true);
+
+				Method methodGetRawHeight = Display.class.getMethod(
+						"getRawHeight");
+				methodGetRawHeight.setAccessible(true);
+				return new Size((Integer)methodGetRawWidth.invoke(d),
+						(Integer)methodGetRawHeight.invoke(d));
+			}
+			catch (Exception e)
+			{
+				Log.e(LOG_TAG + ".GetFullScreenPx", "Error while reflection", e);
+				Point p = new Point();
+				d.getSize(p);
+				return new Size(p.x, p.y);
+			}
+		}
+	}
+
+	private static final String LOG_TAG = DeviceInfo.class.getCanonicalName();
 }
