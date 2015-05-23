112f28e1e5ccedf7cd38e30bddfbe9a7f3c7e636
Ming Ming <mint2648@gmail.com>
2015-05-23 08:59:19 +0800

    Add basic animation time

--- src/main/java/com/nkming/utils/Res.java
+++ src/main/java/com/nkming/utils/Res.java
@@ -12,4 +12,9 @@ public class Res
 {
 	public static final String PACKAGE = "com.nkming.utils";
 	public static final String LOG_TAG = PACKAGE;
+
+	// Standard animation time
+	public static final int ANIMATION_FAST = 200;
+	public static final int ANIMATION_MID = 400;
+	public static final int ANIMATION_SLOW = 500;
 }
