6c9a7c001a705293db62199d0cded8a101008b80
Ming Ming <mint2648@gmail.com>
2015-10-03 16:52:50 +0800

    Add composite iterator to join 2 iterators together

---
+++ src/main/java/com/nkming/utils/type/IteratorComposite.java
@@ -0,0 +1,60 @@
+/*
+ * IteratorComposite.java
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2015 Ming Tsang
+ * Refer to LICENSE for details
+ */
+
+package com.nkming.utils.type;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class IteratorComposite<T> implements Iterator<T>
+{
+	public IteratorComposite(Iterator<T> a, Iterator<T> b)
+	{
+		mItA = a;
+		mItB = b;
+		mItActive = mItA;
+	}
+
+	@Override
+	public boolean hasNext()
+	{
+		return (mItA.hasNext() || mItB.hasNext());
+	}
+
+	@Override
+	public T next()
+	{
+		if (!hasNext())
+		{
+			throw new NoSuchElementException();
+		}
+		else
+		{
+			if (mItA.hasNext())
+			{
+				mItActive = mItA;
+				return mItA.next();
+			}
+			else
+			{
+				mItActive = mItB;
+				return mItB.next();
+			}
+		}
+	}
+
+	@Override
+	public void remove()
+	{
+		mItActive.remove();
+	}
+
+	private Iterator<T> mItA;
+	private Iterator<T> mItB;
+	private Iterator<T> mItActive;
+}
