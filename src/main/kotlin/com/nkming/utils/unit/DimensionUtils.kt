af97d150fb07de5f9334b97021b6ca874e3fb311
Ming Ming <mint2648@gmail.com>
2016-05-22 19:40:41 +0800

    Migrate to kotlin

---
+++ src/main/kotlin/com/nkming/utils/unit/DimensionUtils.kt
@@ -0,0 +1,32 @@
+package com.nkming.utils.unit;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+
+class DimensionUtils
+{
+	companion object
+	{
+		@JvmStatic
+		fun dpToPx(context: Context, dp: Float): Float
+		{
+			val dm = context.resources.displayMetrics
+			return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, dm)
+		}
+
+		@JvmStatic
+		fun spToPx(context: Context, sp: Float): Float
+		{
+			val dm = context.resources.displayMetrics
+			return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, dm)
+		}
+
+		@JvmStatic
+		fun pxToDp(context: Context, px: Float): Float
+		{
+			val dm = context.resources.displayMetrics
+			return px * (DisplayMetrics.DENSITY_DEFAULT / dm.densityDpi.toFloat())
+		}
+	}
+}
