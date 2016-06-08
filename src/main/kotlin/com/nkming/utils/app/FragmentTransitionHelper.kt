572f4fb31d0dd073364f726edc1d8b3c72d27013
Ming Ming <mint2648@gmail.com>
2016-06-08 20:27:04 +0800

    Simplify code

--- src/main/kotlin/com/nkming/utils/app/FragmentTransitionHelper.kt
+++ src/main/kotlin/com/nkming/utils/app/FragmentTransitionHelper.kt
@@ -7,65 +7,62 @@ import android.transition.TransitionInflater
 import android.view.View
 import com.nkming.utils.R
 
-class FragmentTransitionHelper
+object FragmentTransitionHelper
 {
-	companion object
+	fun startTransitionAnimation(context: Context, src: Fragment,
+			srcActivityContainerId: Int, target: Fragment, shared: View,
+			sharedName: String)
 	{
-		fun startTransitionAnimation(context: Context, src: Fragment,
-				srcActivityContainerId: Int, target: Fragment, shared: View,
-				sharedName: String)
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
 		{
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-			{
-				val inflater = TransitionInflater.from(context)
-				src.sharedElementReturnTransition = inflater.inflateTransition(
-						R.transition.change_image_transition)
-				src.exitTransition = inflater.inflateTransition(
-						android.R.transition.fade)
+			val inflater = TransitionInflater.from(context)
+			src.sharedElementReturnTransition = inflater.inflateTransition(
+					R.transition.change_image_transition)
+			src.exitTransition = inflater.inflateTransition(
+					android.R.transition.fade)
 
-				target.sharedElementEnterTransition = inflater.inflateTransition(
-						R.transition.change_image_transition)
-				target.enterTransition = inflater.inflateTransition(
-						android.R.transition.fade)
+			target.sharedElementEnterTransition = inflater.inflateTransition(
+					R.transition.change_image_transition)
+			target.enterTransition = inflater.inflateTransition(
+					android.R.transition.fade)
 
-				src.fragmentManager.beginTransaction()
-						.addSharedElement(shared, sharedName)
-						.replace(srcActivityContainerId, target)
-						.commit()
-			}
-			else
-			{
-				src.fragmentManager.beginTransaction()
-						.setCustomAnimations(android.R.anim.fade_in,
-								android.R.anim.fade_out)
-						.replace(srcActivityContainerId, target)
-						.commit()
-			}
+			src.fragmentManager.beginTransaction()
+					.addSharedElement(shared, sharedName)
+					.replace(srcActivityContainerId, target)
+					.commit()
 		}
-
-		fun startTransitionAnimation(context: Context, src: Fragment,
-				srcActivityContainerId: Int, target: Fragment)
+		else
 		{
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-			{
-				val inflater = TransitionInflater.from(context)
-				src.exitTransition = inflater.inflateTransition(
-						android.R.transition.fade)
-				target.enterTransition = inflater.inflateTransition(
-						android.R.transition.fade)
+			src.fragmentManager.beginTransaction()
+					.setCustomAnimations(android.R.anim.fade_in,
+							android.R.anim.fade_out)
+					.replace(srcActivityContainerId, target)
+					.commit()
+		}
+	}
 
-				src.fragmentManager.beginTransaction()
-						.replace(srcActivityContainerId, target)
-						.commit()
-			}
-			else
-			{
-				src.fragmentManager.beginTransaction()
-						.setCustomAnimations(android.R.anim.fade_in,
-								android.R.anim.fade_out)
-						.replace(srcActivityContainerId, target)
-						.commit()
-			}
+	fun startTransitionAnimation(context: Context, src: Fragment,
+			srcActivityContainerId: Int, target: Fragment)
+	{
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+		{
+			val inflater = TransitionInflater.from(context)
+			src.exitTransition = inflater.inflateTransition(
+					android.R.transition.fade)
+			target.enterTransition = inflater.inflateTransition(
+					android.R.transition.fade)
+
+			src.fragmentManager.beginTransaction()
+					.replace(srcActivityContainerId, target)
+					.commit()
+		}
+		else
+		{
+			src.fragmentManager.beginTransaction()
+					.setCustomAnimations(android.R.anim.fade_in,
+							android.R.anim.fade_out)
+					.replace(srcActivityContainerId, target)
+					.commit()
 		}
 	}
 }
