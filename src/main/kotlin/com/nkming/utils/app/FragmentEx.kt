cc3726ccf059811842ee2d6b4010355eb5a4cd64
Ming Ming <mint2648@gmail.com>
2016-07-05 15:34:14 +0800

    Add view aware code also to native frag

--- src/main/kotlin/com/nkming/utils/app/FragmentEx.kt
+++ src/main/kotlin/com/nkming/utils/app/FragmentEx.kt
@@ -6,7 +6,7 @@ import android.util.SparseArray
 import android.view.View
 import kotlin.reflect.KProperty
 
-open class FragmentEx : Fragment()
+open class FragmentEx : Fragment(), _FragmentViewAwareImpl.Adapter
 {
 	interface LazyView<T : View>
 	{
@@ -38,12 +38,162 @@ open class FragmentEx : Fragment()
 	override fun onActivityCreated(savedInstanceState: Bundle?)
 	{
 		super.onActivityCreated(savedInstanceState)
+		_viewAwareImpl.onActivityCreated(savedInstanceState)
+	}
+
+	/**
+	 * Lazily find a view, and cache the result for later use. The different
+	 * between this method and doing it through lazy delegate, is that we would
+	 * handle the view recreation for you (eg, after your fragment coming back
+	 * from the back stack). This method is not thread-safe
+	 *
+	 * @param id View id
+	 * @return The view or null
+	 */
+	fun <T : View> findViewOrNull(id: Int): T?
+	{
+		return _viewAwareImpl.findViewOrNull(id)
+	}
+
+	/**
+	 * Same as findViewOrNull() but will throw instead of returning null
+	 *
+	 * @param id
+	 * @return
+	 * @see findViewOrNull()
+	 */
+	fun <T : View> findView(id: Int): T
+	{
+		return _viewAwareImpl.findView(id)
+	}
+
+	/**
+	 * @hide
+	 */
+	override val _fragmentViewAwareImpl: _FragmentViewAwareImpl
+		get() = _viewAwareImpl
+
+	/**
+	 * Delegates that would refind the view if the view is recreated
+	 *
+	 * @param id
+	 * @return
+	 */
+	protected fun <T : View> lazyView(id: Int): LazyView<T>
+	{
+		return _viewAwareImpl.lazyView(id)
+	}
+
+	/**
+	 * Delegates that would rerun the initializer if the view is recreated
+	 *
+	 * @param initializer
+	 * @return
+	 */
+	protected fun <T : Any> viewAwareLazy(initializer: () -> T): ViewAwareLazy<T>
+	{
+		return _viewAwareImpl.viewAwareLazy(initializer)
+	}
+
+	private val _viewAwareImpl = object: _FragmentViewAwareImpl()
+	{
+		override fun getView(): View?
+		{
+			return view
+		}
+	}
+}
+
+open class NativeFragmentEx : android.app.Fragment(),
+		_FragmentViewAwareImpl.Adapter
+{
+	override fun onActivityCreated(savedInstanceState: Bundle?)
+	{
+		super.onActivityCreated(savedInstanceState)
+		_viewAwareImpl.onActivityCreated(savedInstanceState)
+	}
+
+	/**
+	 * Lazily find a view, and cache the result for later use. The different
+	 * between this method and doing it through lazy delegate, is that we would
+	 * handle the view recreation for you (eg, after your fragment coming back
+	 * from the back stack). This method is not thread-safe
+	 *
+	 * @param id View id
+	 * @return The view or null
+	 */
+	fun <T : View> findViewOrNull(id: Int): T?
+	{
+		return _viewAwareImpl.findViewOrNull(id)
+	}
+
+	/**
+	 * Same as findViewOrNull() but will throw instead of returning null
+	 *
+	 * @param id
+	 * @return
+	 * @see findViewOrNull()
+	 */
+	fun <T : View> findView(id: Int): T
+	{
+		return _viewAwareImpl.findView(id)
+	}
+
+	/**
+	 * @hide
+	 */
+	override val _fragmentViewAwareImpl: _FragmentViewAwareImpl
+		get() = _viewAwareImpl
+
+	/**
+	 * Delegates that would refind the view if the view is recreated
+	 *
+	 * @param id
+	 * @return
+	 */
+	protected fun <T : View> lazyView(id: Int): FragmentEx.LazyView<T>
+	{
+		return _viewAwareImpl.lazyView(id)
+	}
+
+	/**
+	 * Delegates that would rerun the initializer if the view is recreated
+	 *
+	 * @param initializer
+	 * @return
+	 */
+	protected fun <T : Any> viewAwareLazy(initializer: () -> T)
+			: FragmentEx.ViewAwareLazy<T>
+	{
+		return _viewAwareImpl.viewAwareLazy(initializer)
+	}
+
+	private val _viewAwareImpl = object: _FragmentViewAwareImpl()
+	{
+		override fun getView(): View?
+		{
+			return view
+		}
+	}
+}
+
+abstract class _FragmentViewAwareImpl
+{
+	interface Adapter
+	{
+		val _fragmentViewAwareImpl: _FragmentViewAwareImpl
+	}
+
+	fun onActivityCreated(savedInstanceState: Bundle?)
+	{
 		_views.clear()
 		for (l in _viewAwareLazies)
 		{
 			l.uninit()
 		}
 	}
+
+	abstract fun getView(): View?
 
 	/**
 	 * Lazily find a view, and cache the result for later use. The different
@@ -59,12 +209,14 @@ open class FragmentEx : Fragment()
 		val index = _views.indexOfKey(id)
 		if (index >= 0)
 		{
+			@Suppress("UNCHECKED_CAST")
 			return _views.valueAt(index) as T?
 		}
 		else
 		{
-			val v = view?.findViewById(id)
+			val v = getView()?.findViewById(id)
 			_views.put(id, v)
+			@Suppress("UNCHECKED_CAST")
 			return v as T?
 		}
 	}
@@ -87,7 +239,7 @@ open class FragmentEx : Fragment()
 	 * @param id
 	 * @return
 	 */
-	protected fun <T : View> lazyView(id: Int): LazyView<T>
+	fun <T : View> lazyView(id: Int): FragmentEx.LazyView<T>
 	{
 		return LazyViewImpl(id)
 	}
@@ -98,25 +250,26 @@ open class FragmentEx : Fragment()
 	 * @param initializer
 	 * @return
 	 */
-	protected fun <T : Any> viewAwareLazy(initializer: () -> T): ViewAwareLazy<T>
+	fun <T : Any> viewAwareLazy(initializer: () -> T)
+			: FragmentEx.ViewAwareLazy<T>
 	{
 		val inst = ViewAwareLazyImpl(initializer)
 		_viewAwareLazies.add(inst)
 		return inst
 	}
 
-	private class LazyViewImpl<T : View>(id: Int) : LazyView<T>
+	private class LazyViewImpl<T : View>(id: Int) : FragmentEx.LazyView<T>
 	{
 		override operator fun getValue(thisRef: Any?, property: KProperty<*>): T
 		{
-			return (thisRef as FragmentEx).findView(_id)
+			return (thisRef as Adapter)._fragmentViewAwareImpl.findView(_id)
 		}
 
 		private val _id = id
 	}
 
 	private class ViewAwareLazyImpl<T : Any>(initializer: () -> T)
-			: ViewAwareLazy<T>
+			: FragmentEx.ViewAwareLazy<T>
 	{
 		override operator fun getValue(thisRef: Any?, property: KProperty<*>): T
 		{
@@ -151,5 +304,5 @@ open class FragmentEx : Fragment()
 	}
 
 	private val _views = SparseArray<View?>()
-	private val _viewAwareLazies = arrayListOf<ViewAwareLazy<out Any>>()
+	private val _viewAwareLazies = arrayListOf<FragmentEx.ViewAwareLazy<out Any>>()
 }
